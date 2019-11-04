ca2365038d023d8cec152562cbbc25431a1a822b
sunpriya <sunpriya.bhatia@eduwaive.org>
2019-11-04 12:14:52 +0530

    pages added to crawl

--- crawler_wrapper.py
+++ crawler_wrapper.py
@@ -8,22 +8,27 @@ import argparse
 
 
 #To crawl page
-
-command_to_crawl_page = 'scrapy crawl fb -a email="chaosisfun00@gmail.com" -a password="Justchill@19" -a page="DailyGreaterKashmir" -a date="2018-01-01" -a lang="it" -a  -o posts.csv'
-
-os.system(command_to_crawl_page)
-print("________________PAGE CRAWL COMPLETE.POSTS STARTED_____________")
-
-df = pd.read_csv("/home/sunpriya/fbcrawl/posts.csv")
-posts = df.url
+pages = ['DailyGreaterKashmir','TimesNow','cnnnews18','jandknow','bbcindia','indiatvnews','KashmirReader','KashmirMonitor','IndiaToday','JammuKashmirTV','kashmirnewzservice']
 
 count = 0
-for post in posts:
+for page in pages:
     count = count+1
-    url = "https://mbasic.facebook.com"+post
-    command = 'scrapy crawl comments -a email="chaosisfun00@gmail.com" -a password="Justchill@19" -a post="'+url+'" -o comments'+'.csv'
-    print("_____________CRAWLING POST___________\n"+url)
-    os.system(command)
+    command_to_crawl_page = 'scrapy crawl fb -a email="chaosisfun00@gmail.com" -a password="Justchill@19" -a page="'+page+'" -a date="2019-01-01" -a lang="it" -o posts_'+str(count)+'.csv'
+    os.system(command_to_crawl_page)
+print("________________PAGE CRAWL COMPLETE.POSTS STARTED_____________")
+
+
+for i in range(len(pages)):
+    df = pd.read_csv("/home/sunpriya/fbcrawl/posts_"+str(i+1)+".csv")
+    posts = df.url
+
+    count = 0
+    for post in posts:
+        count = count+1
+        url = "https://mbasic.facebook.com"+post
+        command = 'scrapy crawl comments -a email="chaosisfun00@gmail.com" -a password="Justchill@19" -a post="'+url+'" -o comments'+'.csv'
+        print("_____________CRAWLING POST___________\n"+url)
+        os.system(command)
 
 
 
