997f60062caee1a89246e907bba7204c9ec34fcf
Ubuntu <ubuntu@ip-172-31-94-14.ec2.internal>
2019-12-01 10:05:07 +0000

    credential changes

--- crawler_wrapper.py
+++ crawler_wrapper.py
@@ -11,22 +11,23 @@ import argparse
 pages = ['DailyGreaterKashmir','TimesNow','cnnnews18','jandknow','bbcindia','indiatvnews','KashmirReader','KashmirMonitor','IndiaToday','JammuKashmirTV','kashmirnewzservice']
 
 count = 0
+
 for page in pages:
     count = count+1
-    command_to_crawl_page = 'scrapy crawl fb -a email="chaosisfun00@gmail.com" -a password="Justchill@19" -a page="'+page+'" -a date="2019-01-01" -a lang="it" -o posts_'+str(count)+'.csv'
+    command_to_crawl_page = 'scrapy crawl fb -a email="geekinception17@gmail.com" -a password="rajeevpaji98#" -a page="'+page+'" -a date="2018-01-01" -a lang="it" -o posts_'+str(count)+'.csv'
     os.system(command_to_crawl_page)
 print("________________PAGE CRAWL COMPLETE.POSTS STARTED_____________")
 
 
 for i in range(len(pages)):
-    df = pd.read_csv("/home/sunpriya/fbcrawl/posts_"+str(i+1)+".csv")
+    df = pd.read_csv("posts_"+str(i+1)+".csv")
     posts = df.url
 
     count = 0
     for post in posts:
         count = count+1
         url = "https://mbasic.facebook.com"+post
-        command = 'scrapy crawl comments -a email="chaosisfun00@gmail.com" -a password="Justchill@19" -a post="'+url+'" -o comments'+'.csv'
+        command = 'scrapy crawl comments -a email="geekinception17@gmail.com" -a password="rajeevpaji98#" -a post="'+url+'" -o comments'+'.csv'
         print("_____________CRAWLING POST___________\n"+url)
         os.system(command)
 
